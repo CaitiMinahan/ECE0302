475c7ec2ff6909b974a085238be9d47bc216fc6b
CaitiMinahan <cpm49@pitt.edu>
2022-01-31 20:26:28 -0500

    implement fancy_power.hpp

--- programming_problems/problem03/fancy_power/fancy_power.hpp
+++ programming_problems/problem03/fancy_power/fancy_power.hpp
@@ -3,8 +3,27 @@
 
 int fancy_power(int n, int m)
 {
-	// your code should go here.
-	return 0;
+	//base case: 
+	if(n==0){
+		return 1; //antyhing to the zero power is equal to 1
+	}
+
+	//function calls upon itself 
+	//tests if n is even: 
+	if(n%2==0){
+		m = m*m; //m = m^2 now 
+		n = n/2; //n is now n/2
+		return fancy_power(m, n); //return m^n
+	}
+	//if n is odd: 
+	else{ 
+		m = m*m*m; //m is now m(m^2)
+		n = (n-1)/2; //n is now (n-1)/2
+		return fancy_power(m, n); //return m^n 
+	}
+	//gets us closer to the solution 
+
+	//return 0;
 }
 
 #endif
