39c3be7129bb34842d9a70d1a5b6c05dd50b4034
CaitiMinahan <cpm49@pitt.edu>
2022-01-31 20:44:13 -0500

    wrote test cases

--- programming_problems/problem03/fancy_power/fancy_power_tests.cpp
+++ programming_problems/problem03/fancy_power/fancy_power_tests.cpp
@@ -3,9 +3,29 @@
 
 #include "fancy_power.hpp"
 
-// write your test cases here
-TEST_CASE("Test case", "fancy power"){
-
+//test for even positive powers
+TEST_CASE("Test case for EVEN POSITIVE", "fancy power"){
+  REQUIRE(fancy_power(4, 2)==16); 
+  REQUIRE(true);
+}
+//test for odd positive powers
+TEST_CASE("Test case ODD POSITIVE", "fancy power"){
+  REQUIRE(fancy_power(5, 3)==729); 
+  REQUIRE(true);
+}
+//test for even negative powers
+TEST_CASE("Test casefor EVEN NEGATIVE", "fancy power"){
+  REQUIRE(fancy_power(-4, 2)==(1/16)); 
   REQUIRE(true);
   
+}
+//test for odd negative powers
+TEST_CASE("Test casefor ODD NEGATIVE", "fancy power"){
+  REQUIRE(fancy_power(-5, 3)==(1/729)); 
+  REQUIRE(true);
+}
+//test for zero powers
+TEST_CASE("Test case", "fancy power"){
+  REQUIRE(fancy_power(0, 1)==1); 
+  REQUIRE(true);
 }
