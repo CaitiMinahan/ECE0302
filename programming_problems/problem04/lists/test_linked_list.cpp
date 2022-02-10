76672b275fd869a24f8021ac3b2be01f7bde4d3c
CaitiMinahan <cpm49@pitt.edu>
2022-02-09 19:23:45 -0500

    finished program 4

--- programming_problems/problem04/lists/test_linked_list.cpp
+++ programming_problems/problem04/lists/test_linked_list.cpp
@@ -54,5 +54,43 @@ TEST_CASE( "Tests copy assignment", "[LinkedList]" ) {
   REQUIRE(!L2.isEmpty()); //test is empty   //declare new object 
   L2 = L; //set this new object equal to the first one 
 }
-// TEST_CASE( "Tests copy constructor", "[LinkedList]" ) { }
-// TEST_CASE( "Tests setEntry and getEntry methods", "[LinkedList]" ) { }
+TEST_CASE( "Tests copy constructor", "[LinkedList]" ) { 
+  LinkedList<int> L; //create your object  
+  //assume list: 1 2 3 4 
+  REQUIRE(L.insert(1, 0)); //insert a zero to the 1st position in the list  
+  REQUIRE(L.getLength()==1); 
+  REQUIRE(!L.isEmpty()); 
+  REQUIRE(L.insert(2, 2)); //insert 3 into 2nd position
+  REQUIRE(L.getLength()==2); 
+  REQUIRE(!L.isEmpty()); 
+  REQUIRE(L.insert(3, 3)); //insert a 5 into 3rd position
+  REQUIRE(L.getLength()==3); 
+  REQUIRE(!L.isEmpty()); 
+  REQUIRE(L.insert(4, 4)); //insert a 7 into 5th position
+  REQUIRE(L.getLength()==4); 
+  REQUIRE(!L.isEmpty()); 
+  REQUIRE(L.remove(1)); //remove element from the 3rd position 
+  REQUIRE(L.getLength()==3); //get the length, should be back to original now 
+  L.clear(); //clear the list 
+  REQUIRE(L.isEmpty()); //require it to be empty now 
+  REQUIRE(L.getLength()==0); 
+
+  //declare another object like this: 
+  LinkedList<int> L2 = L; //create your object   
+  //that's it, all the above statements should be included with instantiation of this new object 
+}
+TEST_CASE( "Tests setEntry and getEntry methods", "[LinkedList]" ) { 
+  LinkedList<int> L; //create your object  
+  //assume list: 4 0 4 
+  REQUIRE(L.insert(1, 4)); 
+  REQUIRE(L.getLength()==1);
+  REQUIRE(!L.isEmpty()); 
+  REQUIRE(L.insert(2, 0)); 
+  REQUIRE(L.getLength()==2);
+  REQUIRE(!L.isEmpty()); 
+  REQUIRE(L.insert(3, 4)); 
+  REQUIRE(L.getLength()==3);
+  REQUIRE(!L.isEmpty());   
+  L.setEntry(2,4); //list should now be 4 4 4
+  REQUIRE(L.getEntry(2)==4); //get that item 
+}
