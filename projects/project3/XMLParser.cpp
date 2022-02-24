2b702567d3dfa5b10133c0e9d8f6d15018f88faf
CaitiMinahan <cpm49@pitt.edu>
2022-02-24 15:50:18 -0500

    commit

--- projects/project3/XMLParser.cpp
+++ projects/project3/XMLParser.cpp
@@ -25,13 +25,24 @@ XMLParser::~XMLParser()
 bool XMLParser::tokenizeInputString(const std::string &inputString)
 {	//scan the input string 
 
-	//test all markup (tokens)
+	//store both of these in tokenizeInputVector 
+	TokenStruct token; 	//store token type 
+	std::string tokenName; 	//store string of token
+
+	//tokenizeInputString() should TEST FOR VALID TAGS, including nesting, but should not test for matching start and end tags
+	//so, we have to do two things here: 
+	//1) test valid markup 
+	//2) test valid content 
+
+	//Use elementNameBag to store element names
+
+	//test all markup
 
 	//validity check
 	//start from the beginning of the input string
 	//cannot have nested <>
 
-	//first, test the markup 
+	//1) first, test the markup (anything enclosed in the brackets < >)
 	//determine tag type: START_TAG, END_TAG, EMPTY_TAG, CONTENT, and DECLARATION
 	//include conditions for the specific markup sections (tags)
 		//condition for end tag
@@ -39,10 +50,18 @@ bool XMLParser::tokenizeInputString(const std::string &inputString)
 		//condition for declaration tag 
 		//condition for empty tag 
 		//conditions for content 
-	//save the token name and token type to a TokenStruct (helpful for tokenizeInputvector)	
-	//make sure token names follow the correct format and don't have any weird characters
+	//once we've determined what type of tag we have, save it to your variables
+	//save the token name and token type to a TokenStruct object 	
+		//add conditions where if we found a certain type of a tag in the tests above, update tokenStruct here
 
-	//test the content (stuff between brackets < >) -- content is everything that IS NOT a matkup
+	//now that we have determined the tag name and type, make sure it matches the proper syntax 
+	//and does not have any weird characters that aren't allowed 
+		//add conditions here for what is and isn't allowed for each tag 
+
+	//tokenizedInputvector gets the tag pushed to the stack 
+
+
+	//2) test the content (everything that IS NOT a matkup)
 
 	//return true if tokenization was successful 
 
@@ -58,7 +77,11 @@ static std::string deleteAttributes(std::string input)
 
 // TODO: Implement the parseTokenizedInput method here
 bool XMLParser::parseTokenizedInput()
-{
+{	//Use the stack variable parseStack along with tokenizedInputVector to check for
+	//matching start and end tags while parsing
+	//this is our second half of validating an XML file 
+
+	
 	return false;
 }
 
