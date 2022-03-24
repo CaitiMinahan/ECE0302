2f1cb162e904d8189f699573cc1d5bef7d85dcb4
CaitiMinahan <cpm49@pitt.edu>
2022-03-24 18:54:13 -0400

    commit

--- projects/project4/pathfinder.cpp
+++ projects/project4/pathfinder.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include "string"
+#include "image.cpp"
+#include "deque.hpp"
 
 int main(int argc, char *argv[])
 {
@@ -13,6 +16,29 @@ int main(int argc, char *argv[])
   std::string input_file = argv[1];
   std::string output_file = argv[2];
 
-  // TODO
-  
+  //scan the image recursively until we have reached an exit 
+  //an exit can be found at the edges of the maze (so either max width or max height of the image)
+
+  //read in a PNG file, write to it, and compare the output to the solution PNG file
+
+
+  //conduct a bread-first-search, when we've visited a white space, mark it as visited
+  //keep track of the visited spaces in a deque (to mark our tracks)
+
+
+  //mark the pixel green when we have reached our goal
+  //return EXIT_SUCCESS and print solution Found to standard output
+
+  //consider a pixel with coordinates (r,c):
+
+  //consider a pixel with coordinates (r-1, c) for previousRow: 
+
+  //consider a pixel with coordinates (r+1, c) for nextRow 
+
+  //consider a pixel with coordinates (r, c-1) for previousColumn 
+
+  //consider a pixel with coordinates (r, c+1) for nextColumn:
+
+
+
 }
