1449d17bc6e429b6583ed92212594e817879c3d8
CaitiMinahan <cpm49@pitt.edu>
2022-04-11 12:41:22 -0400

    commit

--- programming_problems/problem10/heap_priority_queue/heap_priority_queue.hpp
+++ programming_problems/problem10/heap_priority_queue/heap_priority_queue.hpp
@@ -30,27 +30,33 @@ private:
 template <typename T>
 bool HeapPriorityQueue<T>::isEmpty()
 {
-    //todo
-    return false;
+    //use composition 
+    return lst.isEmpty();
 }
 
 template <typename T>
 void HeapPriorityQueue<T>::add(const T& item)
 {
-    //todo
+    //use insert method for a max heap 
+    //need to compare entry with items in the list 
+    //make sure it is in descending order (from root to leaves)
+    
+    //keep traversing through the list until we found the proper spot to insert 
+    
 }
 
 template <typename T>
 void HeapPriorityQueue<T>::remove()
 {
-    //todo
+    //reuse with composition
+    lst.remove(0); //remove root
 }
 
 template <typename T>
 T HeapPriorityQueue<T>::peek()
 {
-    //todo
-    return T();
+    //use composition to reuse the getEntry() method from dynamic_array_list
+    return lst.getEntry(0); //get the item from the top, so at position 0
 }
 
 
